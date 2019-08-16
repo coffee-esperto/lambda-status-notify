fe495da20e7648b8a19a692202410dc5f3f785e0
Gabriel Facina <gfacina@uolinc.com>
2019-08-16 17:22:01 -0300

    Criado hook do slack e configurado SNS

--- handler.js
+++ handler.js
@@ -1,14 +1,29 @@
 'use strict';
 
 const dynamoDb = require('./services/dynamoDb');
+const slack = require('./services/slack');
 
 module.exports.coffeNotification = async (event) => {
   const payload = {
-    message: event.Records[0].body,
-    id: event.Records[0].messageId
+    ...JSON.parse(event.Records[0].Sns.Message),
+    id: event.Records[0].Sns.MessageId,
   };
 
+  console.log(payload)
+
   await dynamoDb.put(payload);
+
+  let message = {
+    text: `Não tem café na ${payload.data.location}`,
+    color: 'danger'
+  }
+
+  if (payload.data.hasCoffee) {
+    message.text = `Tem café na ${payload.data.location}`;
+    message.color = 'good';
+  }
+  
+  await slack.send(message);
 
   return {
     statusCode: 200,
---
+++ services/slack.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+const send = async (body) => {
+    return fetch('https://hooks.slack.com/services/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', {
+        method: 'post',
+        body:    JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+
+module.exports = {
+    send,
+};
